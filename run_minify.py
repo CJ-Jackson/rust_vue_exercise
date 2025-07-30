1da07e11d3bc7368d048975d6fad31c284ab562c
Chris Jackson <hello@cjjackson.dev>
2025-07-30 12:50:30 +0100

    feat: added run_minify.py

---
+++ run_minify.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+from pathlib import Path
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# go install github.com/tdewolff/minify/cmd/minify@latest
+
+for item in Path('src').glob('**/_asset/*.js').__iter__():
+    if str(item).endswith('min.js'):
+        continue
+    output = item.with_name(item.stem + '.min.js')
+    subprocess.run(['minify', '-o', output, item])
+    print(output)
+
+for item in Path('src').glob('**/_asset/*.css').__iter__():
+    if str(item).endswith('min.js'):
+        continue
+    output = item.with_name(item.stem + '.min.css')
+    subprocess.run(['minify', '-o', output, item])
+    print(output)
